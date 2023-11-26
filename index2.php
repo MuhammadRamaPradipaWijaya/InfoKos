23c428be9415d029ff3e6f6c3053a05f08249de8
adtyridho <144533561+adtyridho@users.noreply.github.com>
2023-11-26 22:18:38 +0700

    ppp

--- index2.php
+++ index2.php
@@ -434,7 +434,7 @@ function minfas($idkost, $tipe_kost)
       <div class="container">
         <div class="row text-center">
           <div class="col-md-8 mb-3 mb-md-0 mx-auto">
-            <p>Copyright &copy;<script>document.write(new Date().getFullYear());</script>. All Rights Reserved. &mdash; Designed with love by <a href="https://untree.co" class="link-highlight">Untree.co</a> <!-- License information: https://untree.co/license/ -->Distributed By <a href="https://themewagon.com" target="_blank" >ThemeWagon</a>
+            <p>Copyright &copy;<script>document.write(new Date().getFullYear());</script>. All Rights Reserved. &mdash; Designed with love by <a href="https://untree.co" class="link-highlight">Untree.co</a> 
             </p>
           </div>
           
