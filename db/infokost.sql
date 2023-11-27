c40f6599ef8897ad043aa0fb8a1dfdd53d2a6066
Muhammad Rama Pradipa Wijaya <ramapradipa37@gmail.com>
2023-11-27 13:28:08 +0700

    commit

--- db/infokost.sql
+++ db/infokost.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 21, 2023 at 03:20 AM
+-- Generation Time: Nov 27, 2023 at 07:19 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `infokos`
+-- Database: `infokost`
 --
 
 -- --------------------------------------------------------
@@ -36,7 +36,25 @@ CREATE TABLE `booking` (
   `durasi_sewa` int(11) NOT NULL,
   `tanggal_keluar` date NOT NULL,
   `jumlah_kamar` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `booking`
+--
+
+INSERT INTO `booking` (`id_booking`, `id_user`, `id_kamar`, `tanggal_masuk`, `hitungan_sewa`, `durasi_sewa`, `tanggal_keluar`, `jumlah_kamar`) VALUES
+(6, 3, 1, '2023-12-01', 3, 1, '2024-01-01', 1);
+
+--
+-- Triggers `booking`
+--
+DELIMITER $$
+CREATE TRIGGER `update_kamar` AFTER INSERT ON `booking` FOR EACH ROW BEGIN
+	UPDATE kamar SET jumlah_kamar=jumlah_kamar-NEW.jumlah_kamar
+    WHERE id_kamar=NEW.id_kamar;
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -53,7 +71,37 @@ CREATE TABLE `kamar` (
   `tipe_kamar` varchar(255) NOT NULL,
   `biaya_fasilitas` int(11) NOT NULL,
   `fasilitas_kamar` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `kamar`
+--
+
+INSERT INTO `kamar` (`id_kamar`, `id_kost`, `jumlah_kamar`, `panjang_kamar`, `lebar_kamar`, `tipe_kamar`, `biaya_fasilitas`, `fasilitas_kamar`) VALUES
+(1, 11, 4, 30, 30, 'kamar mandi dalam', 0, 'Tempat Tidur, Lemari, Kipas Angin');
+
+--
+-- Triggers `kamar`
+--
+DELIMITER $$
+CREATE TRIGGER `after_update` AFTER UPDATE ON `kamar` FOR EACH ROW BEGIN
+	UPDATE kost SET jumlah_kamar=jumlah_kamar-1
+	WHERE id_kost=NEW.id_kost;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `hapus_kamar` AFTER DELETE ON `kamar` FOR EACH ROW UPDATE kost SET kost.jumlah_kamar = kost.jumlah_kamar-old.jumlah_kamar
+WHERE kost.id_kost=old.id_kost
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `total_kamar` AFTER INSERT ON `kamar` FOR EACH ROW BEGIN
+	UPDATE kost SET jumlah_kamar=jumlah_kamar+NEW.jumlah_kamar
+	WHERE id_kost=NEW.id_kost;
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -85,14 +133,14 @@ CREATE TABLE `kost` (
   `deskripsi` text NOT NULL,
   `id_pemilik` int(11) NOT NULL,
   `fasilitas_kost` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `kost`
 --
 
 INSERT INTO `kost` (`id_kost`, `nama_kost`, `tipe_kost`, `jenis_kost`, `jumlah_kamar`, `tanggal_tagih`, `nama_pemilik`, `nama_bank`, `no_rekening`, `foto_bangunan_utama`, `foto_kamar`, `foto_kamar_mandi`, `foto_interior`, `provinsi`, `kota`, `kecamatan`, `kelurahan`, `alamat`, `harga_sewa`, `kontak`, `deskripsi`, `id_pemilik`, `fasilitas_kost`) VALUES
-(1, 'kost 1', 'bulan', 'putri', 10, '2023-11-21', 'Pak kos', 'Mandiri', 12345678, 'contoh.jpg', 'contoh.jpg', 'contoh.jpg', 'contoh.jpg', 'Jawa Timur', 'Jember', 'Sumbersari', 'Tegalgede', 'Jl. Mastrip', 350000, '082139425332', 'Kost Muslim', 2, 'WIFI');
+(11, 'Kost 1A', 'Bulan', 'Putra', 3, '2023-12-01', 'owner', 'BRI', 2147483647, 'kos.jpeg', 'kos.jpeg', 'kos.jpeg', 'kos.jpeg', 'Jawa Timur', 'Jember', 'Sumbersari', 'Tegalgede', 'Jl. Mastrip', 500000, '082139425332', '', 2, 'WIFI/Internet, Ruang Tamu, Dapur, Musholla');
 
 -- --------------------------------------------------------
 
@@ -102,10 +150,10 @@ INSERT INTO `kost` (`id_kost`, `nama_kost`, `tipe_kost`, `jenis_kost`, `jumlah_k
 
 CREATE TABLE `rating` (
   `id_rating` int(11) NOT NULL,
-  `id_kost` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
+  `Id_kost` int(11) NOT NULL,
+  `Id_user` int(11) NOT NULL,
   `score` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -116,7 +164,7 @@ CREATE TABLE `rating` (
 CREATE TABLE `roles_user` (
   `id_roles` int(11) NOT NULL,
   `nama` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `roles_user`
@@ -140,7 +188,14 @@ CREATE TABLE `tagihan` (
   `stats` int(11) NOT NULL,
   `tanggal_tagihan` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `bukti_bayar` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tagihan`
+--
+
+INSERT INTO `tagihan` (`no_tagihan`, `no_booking`, `total_tagihan`, `stats`, `tanggal_tagihan`, `bukti_bayar`) VALUES
+(1, 6, 500000, 1, '2023-11-27 06:19:15', 'hero-slider-2.jpg');
 
 -- --------------------------------------------------------
 
@@ -156,20 +211,21 @@ CREATE TABLE `user` (
   `password` varchar(255) NOT NULL,
   `no_hp` varchar(255) NOT NULL,
   `pekerjaan` varchar(255) NOT NULL,
-  `jenis_kelamin` varchar(255) NOT NULL,
+  `jenis_kelamin` varchar(25) NOT NULL,
   `foto_ktp` varchar(255) NOT NULL,
   `foto_profil` text NOT NULL,
   `roles` int(11) NOT NULL,
   `id_kost_saya` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `nama_lengkap`, `email`, `username`, `password`, `no_hp`, `pekerjaan`, `jenis_kelamin`, `foto_ktp`, `foto_profil`, `roles`, `id_kost_saya`) VALUES
-(1, 'Admin1', 'admin1@mail.com', 'admin', 'admin', '082239435432', 'Administrator', 'Laki-laki', '-', '-', 3, 0),
-(2, 'owner1', 'owner1@mail.com', 'owner1', 'owner1', '082139425332', 'Pemilik Kos', 'Laki-laki', '-', '-', 2, 1);
+(1, 'admin', 'admin@mail.com', 'admin', 'admin', '081234567890', 'administrator', 'Laki-laki', '-', '-', 3, 0),
+(2, 'owner', 'owner@mail.com', 'owner', 'owner', '82139425332', 'Pemilik kos', 'laki-laki', '20522103.jpg', 'images.jpg', 2, 0),
+(3, 'user', 'user@mail.com', 'user', 'user', '082129921222', 'Mahasiswa', ' laki-laki', '20522103.jpg', 'images.jpg', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -181,7 +237,14 @@ CREATE TABLE `wishlist` (
   `id_wishlist` int(11) NOT NULL,
   `id_kost` int(11) NOT NULL,
   `id_user` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `wishlist`
+--
+
+INSERT INTO `wishlist` (`id_wishlist`, `id_kost`, `id_user`) VALUES
+(2, 11, 3);
 
 --
 -- Indexes for dumped tables
@@ -192,7 +255,8 @@ CREATE TABLE `wishlist` (
 --
 ALTER TABLE `booking`
   ADD PRIMARY KEY (`id_booking`),
-  ADD KEY `id_user` (`id_user`,`id_kamar`);
+  ADD KEY `id_user` (`id_user`,`id_kamar`),
+  ADD KEY `id_kamar` (`id_kamar`);
 
 --
 -- Indexes for table `kamar`
@@ -213,8 +277,8 @@ ALTER TABLE `kost`
 --
 ALTER TABLE `rating`
   ADD PRIMARY KEY (`id_rating`),
-  ADD KEY `id_kost` (`id_kost`,`id_user`),
-  ADD KEY `id_user` (`id_user`);
+  ADD KEY `Id_kost` (`Id_kost`),
+  ADD KEY `Id_user` (`Id_user`);
 
 --
 -- Indexes for table `roles_user`
@@ -234,6 +298,7 @@ ALTER TABLE `tagihan`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
+  ADD KEY `id_kost_saya` (`id_kost_saya`),
   ADD KEY `roles` (`roles`);
 
 --
@@ -241,7 +306,7 @@ ALTER TABLE `user`
 --
 ALTER TABLE `wishlist`
   ADD PRIMARY KEY (`id_wishlist`),
-  ADD KEY `id_kost` (`id_kost`,`id_user`),
+  ADD KEY `id_kost` (`id_kost`),
   ADD KEY `id_user` (`id_user`);
 
 --
@@ -252,19 +317,19 @@ ALTER TABLE `wishlist`
 -- AUTO_INCREMENT for table `booking`
 --
 ALTER TABLE `booking`
-  MODIFY `id_booking` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_booking` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `kamar`
 --
 ALTER TABLE `kamar`
-  MODIFY `id_kamar` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_kamar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `kost`
 --
 ALTER TABLE `kost`
-  MODIFY `id_kost` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_kost` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `rating`
@@ -282,19 +347,19 @@ ALTER TABLE `roles_user`
 -- AUTO_INCREMENT for table `tagihan`
 --
 ALTER TABLE `tagihan`
-  MODIFY `no_tagihan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `no_tagihan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT for table `wishlist`
 --
 ALTER TABLE `wishlist`
-  MODIFY `id_wishlist` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_wishlist` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -304,8 +369,8 @@ ALTER TABLE `wishlist`
 -- Constraints for table `booking`
 --
 ALTER TABLE `booking`
-  ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`),
-  ADD CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`id_booking`) REFERENCES `tagihan` (`no_booking`);
+  ADD CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `booking_ibfk_3` FOREIGN KEY (`id_kamar`) REFERENCES `kamar` (`id_kamar`);
 
 --
 -- Constraints for table `kamar`
@@ -323,13 +388,27 @@ ALTER TABLE `kost`
 -- Constraints for table `rating`
 --
 ALTER TABLE `rating`
-  ADD CONSTRAINT `rating_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
+  ADD CONSTRAINT `rating_ibfk_1` FOREIGN KEY (`Id_kost`) REFERENCES `kost` (`id_kost`),
+  ADD CONSTRAINT `rating_ibfk_2` FOREIGN KEY (`Id_user`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `tagihan`
+--
+ALTER TABLE `tagihan`
+  ADD CONSTRAINT `tagihan_ibfk_1` FOREIGN KEY (`no_booking`) REFERENCES `booking` (`id_booking`);
 
 --
 -- Constraints for table `user`
 --
 ALTER TABLE `user`
   ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`roles`) REFERENCES `roles_user` (`id_roles`);
+
+--
+-- Constraints for table `wishlist`
+--
+ALTER TABLE `wishlist`
+  ADD CONSTRAINT `wishlist_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `wishlist_ibfk_2` FOREIGN KEY (`id_kost`) REFERENCES `kost` (`id_kost`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
