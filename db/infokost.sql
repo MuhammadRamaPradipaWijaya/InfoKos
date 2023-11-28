2043b6224a648b8461d45f03aaa4667afd36aec6
Erakeshh <144751538+Erakeshh@users.noreply.github.com>
2023-11-28 13:15:14 +0700

    update database

--- db/infokost.sql
+++ db/infokost.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2023 at 04:05 AM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Generation Time: Nov 28, 2023 at 07:14 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,7 +36,7 @@ CREATE TABLE `booking` (
   `durasi_sewa` int(11) NOT NULL,
   `tanggal_keluar` date NOT NULL,
   `jumlah_kamar` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Triggers `booking`
@@ -64,16 +64,21 @@ CREATE TABLE `kamar` (
   `tipe_kamar` varchar(255) NOT NULL,
   `biaya_fasilitas` int(11) NOT NULL,
   `fasilitas_kamar` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `kamar`
 --
 
 INSERT INTO `kamar` (`id_kamar`, `id_kost`, `jumlah_kamar`, `panjang_kamar`, `lebar_kamar`, `tipe_kamar`, `biaya_fasilitas`, `fasilitas_kamar`) VALUES
-(8, 15, 6, 5, 4, 'kamar mandi dalam', 0, 'Tempat Tidur, Lemari, Kipas Angin'),
-(9, 16, 25, 5, 4, 'kamar mandi luar', 0, 'Tempat Tidur, Lemari, TV, Kulkas, Kipas Angin'),
-(10, 17, 11, 5, 4, 'kamar mandi luar', 0, 'Tempat Tidur, Lemari, Kulkas, Kipas Angin');
+(14, 15, 4, 5, 4, 'kamar mandi luar', 450000, 'Tempat Tidur, Lemari, Kipas Angin'),
+(15, 15, 2, 5, 4, 'kamar mandi dalam', 500000, 'Tempat Tidur, Lemari, Kipas Angin'),
+(16, 16, 21, 5, 4, 'kamar mandi luar', 425000, 'Tempat Tidur, Lemari, TV, Kulkas, Kipas Angin'),
+(17, 16, 4, 10, 8, 'kamar mandi luar', 650000, 'Tempat Tidur, Lemari, TV, Kulkas, Kipas Angin'),
+(18, 17, 9, 5, 4, 'kamar mandi luar', 400000, 'Tempat Tidur, Lemari, Kulkas, Kipas Angin'),
+(19, 17, 2, 5, 4, 'kamar mandi dalam', 500000, 'Tempat Tidur, Lemari, Kulkas, Kipas Angin'),
+(20, 18, 2, 5, 4, 'kamar mandi luar', 400000, 'AC, Tempat Tidur, Lemari, Kulkas'),
+(21, 18, 3, 5, 4, 'kamar mandi dalam', 500000, 'AC, Tempat Tidur, Lemari, Kulkas');
 
 --
 -- Triggers `kamar`
@@ -128,16 +133,17 @@ CREATE TABLE `kost` (
   `deskripsi` text NOT NULL,
   `id_pemilik` int(11) NOT NULL,
   `fasilitas_kost` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `kost`
 --
 
 INSERT INTO `kost` (`id_kost`, `nama_kost`, `tipe_kost`, `jenis_kost`, `jumlah_kamar`, `tanggal_tagih`, `nama_pemilik`, `nama_bank`, `no_rekening`, `foto_bangunan_utama`, `foto_kamar`, `foto_kamar_mandi`, `foto_interior`, `provinsi`, `kota`, `kecamatan`, `kelurahan`, `alamat`, `harga_sewa`, `kontak`, `deskripsi`, `id_pemilik`, `fasilitas_kost`) VALUES
-(15, 'Kos Semeru', 'Bulan', 'Putra', 5, '2023-11-01', 'Iqbal', 'BNI', 872534765, 'IMG-20231128-WA0010.jpg', 'IMG-20231128-WA0011.jpg', 'IMG-20231128-WA0012.jpg', 'IMG-20231128-WA0009.jpg', 'Jawa Timur', 'Jember', 'Sumbersari', 'Sumbersari', 'Jl. Semeru no. 03', 500000, '082338636603', '', 46, 'Parkir Mobil, WIFI/Internet'),
-(16, 'Kos Putri Calysta ', 'Bulan', 'Putri', 25, '2023-11-01', 'Aisyah', 'BNI', 2147483647, 'o70ibRGM0DSJD4CaEIxGj24rz5bB3RopABJcnp8XiPo=_plaintext_638367343263134160.jpg', 'k04_MT4qwS2HqT--8lSzym0ZEgirJ5-0UY_E51dT1-0=_plaintext_638367343289062873.jpg', 'FZeJczIMbN_z5jHJyfqyObPbXl9qW0yxk9nNxQUtc1o=_plaintext_638367343269639410.jpg', 'SCoflmCxIzK5AUIAvOxt_9rOD5pkkSNrf8oRONS9H3o=_plaintext_638367343293230416.jpg', 'Jawa Timur', 'Jember', 'Sumbersari', 'Sumbersari', 'Jl. Mastrip Gg. 2 no.14', 425000, '085267322720', 'Kost Putri ', 47, 'Parkir Mobil, WIFI/Internet, Ruang Makan, Dapur'),
-(17, 'Kos Istana Tidar', 'Bulan', 'Putra', 11, '2023-11-01', 'Sena', 'BRI', 2147483647, 'IMG-20231128-WA0020.jpg', 'IMG-20231128-WA0022.jpg', 'IMG-20231128-WA0019.jpg', 'IMG-20231128-WA0021.jpg', 'Jawa Timur', 'Jember', 'Sumbersari', 'Sumbersari', 'Perum Istana Tidar B4 no.23', 400000, '081935166922', 'Kos Laki-Laki', 48, 'Parkir Mobil, WIFI/Internet, Ruang Tamu, Ruang Makan, Dapur');
+(15, 'Kos Semeru', 'Bulan', 'Putra', 1, '2023-11-01', 'Iqbal', 'BNI', 872534765, 'IMG-20231128-WA0010.jpg', 'IMG-20231128-WA0011.jpg', 'IMG-20231128-WA0012.jpg', 'IMG-20231128-WA0009.jpg', 'Jawa Timur', 'Jember', 'Sumbersari', 'Sumbersari', 'Jl. Semeru no. 03', 0, '082338636603', 'Kos Laki-Laki', 46, 'Parkir Mobil, WIFI/Internet'),
+(16, 'Kos Putri Calysta ', 'Bulan', 'Putri', 22, '2023-11-01', 'Aisyah', 'BNI', 2147483647, 'o70ibRGM0DSJD4CaEIxGj24rz5bB3RopABJcnp8XiPo=_plaintext_638367343263134160.jpg', 'k04_MT4qwS2HqT--8lSzym0ZEgirJ5-0UY_E51dT1-0=_plaintext_638367343289062873.jpg', 'FZeJczIMbN_z5jHJyfqyObPbXl9qW0yxk9nNxQUtc1o=_plaintext_638367343269639410.jpg', 'SCoflmCxIzK5AUIAvOxt_9rOD5pkkSNrf8oRONS9H3o=_plaintext_638367343293230416.jpg', 'Jawa Timur', 'Jember', 'Sumbersari', 'Sumbersari', 'Jl. Mastrip Gg. 2 no.14', 0, '085267322720', 'Kost Putri \r\n21 kamar perorang.\r\n4 kamar isi 2 orang', 47, 'Parkir Mobil, WIFI/Internet, Ruang Makan, Dapur'),
+(17, 'Kos Istana Tidar', 'Bulan', 'Putra', 10, '2023-11-01', 'Sena', 'BRI', 2147483647, 'IMG-20231128-WA0020.jpg', 'IMG-20231128-WA0022.jpg', 'IMG-20231128-WA0019.jpg', 'IMG-20231128-WA0021.jpg', 'Jawa Timur', 'Jember', 'Sumbersari', 'Sumbersari', 'Perum Istana Tidar B4 no.23', 0, '081935166922', 'Kos Laki-Laki\r\n9 kamar mandi luar\r\n2 kamar mandi dalam\r\nfree wifi', 48, 'Parkir Mobil, WIFI/Internet, Ruang Tamu, Ruang Makan, Dapur'),
+(18, 'Rey Kos', 'Bulan', 'Putra', 4, '2023-11-01', 'Reynand', 'BRI', 23879654, 'IMG-20231128-WA0017.jpg', 'IMG-20231128-WA0015.jpg', 'IMG-20231128-WA0014.jpg', 'IMG-20231128-WA0018.jpg', 'Jawa Timur', 'Jember', 'Mangli', 'Mangli', 'Perum Mangli ', 0, '082332606541', 'Kost Laki-Laki', 50, 'Parkir Mobil, WIFI/Internet, Ruang Tamu, Ruang Makan, Dapur');
 
 -- --------------------------------------------------------
 
@@ -150,7 +156,7 @@ CREATE TABLE `rating` (
   `Id_kost` int(11) NOT NULL,
   `Id_user` int(11) NOT NULL,
   `score` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -161,7 +167,7 @@ CREATE TABLE `rating` (
 CREATE TABLE `roles_user` (
   `id_roles` int(11) NOT NULL,
   `nama` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `roles_user`
@@ -185,7 +191,7 @@ CREATE TABLE `tagihan` (
   `stats` int(11) NOT NULL,
   `tanggal_tagihan` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `bukti_bayar` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -200,23 +206,22 @@ CREATE TABLE `user` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `no_hp` varchar(255) NOT NULL,
-  `pekerjaan` varchar(255) NOT NULL,
-  `jenis_kelamin` varchar(25) NOT NULL,
   `foto_ktp` varchar(255) NOT NULL,
   `foto_profil` text NOT NULL,
   `roles` int(11) NOT NULL,
   `id_kost_saya` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `nama_lengkap`, `email`, `username`, `password`, `no_hp`, `pekerjaan`, `jenis_kelamin`, `foto_ktp`, `foto_profil`, `roles`, `id_kost_saya`) VALUES
-(46, 'IQBAL AMIN', 'iqbal@gmail.com', 'Iqbal', 'Iqbal', '82338636603', 'Ow', ' laki-laki', '', 'IQBAL AMIN.jpeg', 2, 0),
-(47, 'Aisyah.S', 'aisyah@gmail.com', 'Aisyah', 'Aisyah', '85267322720', 'owner Kos', ' perempuan', '', 'WhatsApp Image 2023-11-28 at 09.30.11_d2e82ac8.jpg', 2, 0),
-(48, 'Arya Advicenna', 'sena@gmail.com', 'Sena', 'Sena', '81935166922', 'owner Kos', ' laki-laki', '', 'WhatsApp Image 2023-11-28 at 09.34.39_480510a3.jpg', 2, 0),
-(49, 'user', 'user@gmail.com', 'user', 'user', '98765432', 'mahasiswa', 'laki-laki', 'bg1.jpg', 'bg1.jpg', 1, 0);
+INSERT INTO `user` (`id`, `nama_lengkap`, `email`, `username`, `password`, `no_hp`, `foto_ktp`, `foto_profil`, `roles`, `id_kost_saya`) VALUES
+(46, 'IQBAL AMIN', 'iqbal@gmail.com', 'Iqbal', 'Iqbal', '82338636603', '', 'IQBAL AMIN.jpeg', 2, 0),
+(47, 'Aisyah.S', 'aisyah@gmail.com', 'Aisyah', 'Aisyah', '85267322720', '', 'WhatsApp Image 2023-11-28 at 09.30.11_d2e82ac8.jpg', 2, 0),
+(48, 'Arya Advicenna', 'sena@gmail.com', 'Sena', 'Sena', '81935166922', '', 'WhatsApp Image 2023-11-28 at 09.34.39_480510a3.jpg', 2, 0),
+(49, 'user', 'user@gmail.com', 'user', 'user', '98765432', 'bg1.jpg', 'bg1.jpg', 1, 0),
+(50, 'Reynand', 'reynand@gmail.com', 'Reynand', 'Reynand', '082332606541', '', 'WhatsApp Image 2023-11-28 at 10.19.27_acd4f172.jpg', 2, 0);
 
 -- --------------------------------------------------------
 
@@ -228,7 +233,7 @@ CREATE TABLE `wishlist` (
   `id_wishlist` int(11) NOT NULL,
   `id_kost` int(11) NOT NULL,
   `id_user` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Indexes for dumped tables
@@ -307,13 +312,13 @@ ALTER TABLE `booking`
 -- AUTO_INCREMENT for table `kamar`
 --
 ALTER TABLE `kamar`
-  MODIFY `id_kamar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_kamar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `kost`
 --
 ALTER TABLE `kost`
-  MODIFY `id_kost` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id_kost` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `rating`
@@ -337,7 +342,7 @@ ALTER TABLE `tagihan`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT for table `wishlist`
