45367d5506f2f43654cdb783b3307102d84042b2
Muhammad Rama Pradipa Wijaya <ramapradipa37@gmail.com>
2023-11-29 10:27:13 +0700

    update

--- kost/php/properti-edit_proses.php
+++ kost/php/properti-edit_proses.php
@@ -48,43 +48,62 @@ if (isset($_POST['ubah'])) {
     $foto_3 = $_FILES['foto_kamar_mandi']['tmp_name'];
     $foto_4 = $_FILES['foto_interior']['tmp_name'];
 
+    // ... (previous code)
+
     // move uploaded untuk pindahkan data
     if ($foto_bangunan_utama != "") {
         move_uploaded_file($foto_1, "../../img/foto_kost/bangunan_utama/" . $foto_bangunan_utama);
-        move_uploaded_file($foto_2, "../../img/foto_kost/kamar/" . $foto_kamar);
-        move_uploaded_file($foto_3, "../../img/foto_kost/kamar_mandi/" . $foto_kamar_mandi);
-        move_uploaded_file($foto_4, "../../img/foto_kost/interior/" . $foto_interior);
     } else {
         $foto_bangunan_utama = $n['foto_bangunan_utama'];
+    }
+
+    if ($foto_kamar != "") {
+        move_uploaded_file($foto_2, "../../img/foto_kost/kamar/" . $foto_kamar);
+    } else {
         $foto_kamar = $n['foto_kamar'];
+    }
+
+    if ($foto_kamar_mandi != "") {
+        move_uploaded_file($foto_3, "../../img/foto_kost/kamar_mandi/" . $foto_kamar_mandi);
+    } else {
         $foto_kamar_mandi = $n['foto_kamar_mandi'];
+    }
+
+    if ($foto_interior != "") {
+        move_uploaded_file($foto_4, "../../img/foto_kost/interior/" . $foto_interior);
+    } else {
         $foto_interior = $n['foto_interior'];
     }
+
+    // ... (continue with the rest of your code)
+
 
     // detail kost 
     $jenis_kost = $_POST['jenis_kost'];
     $tipe_kost = $_POST['tipe_kost'];
     $harga_sewa = $_POST['harga_sewa'];
-    
-    //id pemilik
+
+    // id pemilik
     $username = $_SESSION['username'];
     $data = mysqli_query($koneksi, "SELECT * FROM user WHERE username='$username'");
     $d = mysqli_fetch_array($data);
     $pemilik = $d['id'];
-    //end insilaisasi
+    // end inisialisasi
 
-    //query insertjumlah
+    // query insert jumlah
     $query = "UPDATE kost SET nama_kost='$nama_kost',tipe_kost='$tipe_kost',jenis_kost='$jenis_kost',tanggal_tagih='$tanggal_tagih',nama_pemilik='$nama_pemilik',nama_bank='$nama_bank',no_rekening='$no_rekening',foto_bangunan_utama='$foto_bangunan_utama',foto_kamar='$foto_kamar',foto_kamar_mandi='$foto_kamar_mandi',foto_interior='$foto_interior',provinsi='$provinsi',kota='$kota',kecamatan='$kecamatan',kelurahan='$kelurahan',alamat='$alamat',harga_sewa='$harga_sewa',kontak='$kontak',deskripsi='$deskripsi',fasilitas_kost='$fasilitas' WHERE id_kost=$id_kost";
     $tambah = mysqli_query($koneksi, $query);
+
     $query_ambil = mysqli_query($koneksi, "SELECT id_kost FROM kost WHERE nama_kost='$nama_kost'");
     $g = mysqli_fetch_array($query_ambil);
     $id_kost = $g['id_kost'];
+
     if ($tambah) {
-        header("location:../properti.php");
+        header("location:../properti.php?id_kost=$id_kost");
     } else {
         header("location:../index.php");
         echo "<script>alert('gagal')</script>";
-    }
-}
 
+        }
+    }
 ?>
