dd87969e70d80a1f0e2fc3d043c421d6d43e68a1
Muhammad Rama Pradipa Wijaya <ramapradipa37@gmail.com>
2023-11-29 10:44:42 +0700

    update

--- kost/php/validasi_proses.php
+++ kost/php/validasi_proses.php
@@ -1,11 +1,27 @@
 <?php
 include "../../koneksi.php";
 
-$id_tagihan = $_POST['id_tagihan'];
+// Check if 'id_tagihan' is set in the POST data
+if (isset($_POST['id_tagihan'])) {
+    $id_tagihan = $_POST['id_tagihan'];
 
-if (isset($_POST['submit'])) {
+    // Update the 'stats' field in the 'tagihan' table
     $query = "UPDATE tagihan SET stats=1 WHERE no_tagihan='$id_tagihan'";
     $data = mysqli_query($koneksi, $query);
 
-    header("location:../tagihan.php");
+    // Check if the update was successful
+    if ($data) {
+        // Redirect to tagihan.php if successful
+        header("location:../tagihan.php");
+        exit();
+    } else {
+        // Handle the case where the update fails
+        echo "Error updating tagihan: " . mysqli_error($koneksi);
+        // You might want to handle the error more gracefully, e.g., display a user-friendly message
+    }
+} else {
+    // Handle the case where 'id_tagihan' is not set in the POST data
+    echo "Invalid request. Please provide 'id_tagihan'.";
+    // You might want to redirect or display an error message to the user
 }
+?>
