aac6b51baff31db736c1a23b068ed899ef4d9a4f
Muhammad Rama Pradipa Wijaya <ramapradipa37@gmail.com>
2023-12-01 07:10:46 +0700

    Update header.php

--- kost/includes/header.php
+++ kost/includes/header.php
@@ -203,7 +203,21 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline text-gray-800 small"><?php echo $_SESSION['username']; ?></span>
+                                <img class="img-profile rounded-circle" src="../img/profil/<?php echo $d['foto_profil'] ?>" alt="Profile Picture">
                             </a>
+                        <!-- Dropdown - User Information -->
+                        <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                            <a class="dropdown-item" href="profil.php">
+                            <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Profil
+                            </a>
+
+                            <!--<a class="dropdown-item" href="logout.php" data-toggle="modal" data-target="#logout">
+                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Logout
+                            </a>-->
+
+                        </div>
                         </li>
 
                     </ul>
