c24eb56b65a3472c952d65e8671c4620e2766422
Muhammad Rama Pradipa Wijaya <ramapradipa37@gmail.com>
2023-12-02 21:26:15 +0700

    Update index2.php

--- index2.php
+++ index2.php
@@ -53,12 +53,6 @@ function minfas($idkost, $tipe_kost)
   <link rel="stylesheet" href="css/daterangepicker.css">
   <link rel="stylesheet" href="css/aos.css">
   <link rel="stylesheet" href="css/style.css">
-
-  <!-- Vendor CSS Files -->
-  <!--<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">-->
-  <!--<link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">-->
-  <!--<link href="assets/vendor/aos/aos.css" rel="stylesheet">-->
-  <!--<link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">-->
   <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
@@ -355,8 +349,6 @@ function minfas($idkost, $tipe_kost)
 
   
   <!-- Vendor JS Files -->
-  <!--<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>-->
-  <!--<script src="assets/vendor/aos/aos.js"></script>-->
   <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
   <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
   <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
